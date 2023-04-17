74276080a7c6fc283f8cb4708b545cfb08c8efd2
Nuta <97901550+nuta-ad@users.noreply.github.com>
2023-04-17 19:52:30 +0500

    Delete web.py

--- data/web.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, StringField
-
-
-class HelpingForm(FlaskForm):
-    problem = TextAreaField("Опишите Вашу проблему")
-    geo = StringField("Ваш адрес")
-    number = StringField("Ваш номер")
-    submit = SubmitField('Отправить')
